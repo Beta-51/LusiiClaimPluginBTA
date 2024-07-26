def0c2b5e38c28d01e5c185bfb7d14ec4807d8b5
dejav <g@g.com>
2024-07-26 15:48:48 -0400

    updated to release 7.2

--- src/main/java/lusii/lusiiclaimchunks/mixins/NetServerHandlerMixin.java
+++ src/main/java/lusii/lusiiclaimchunks/mixins/NetServerHandlerMixin.java
@@ -15,6 +15,7 @@ import net.minecraft.server.world.WorldServer;
 import org.checkerframework.checker.units.qual.A;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
